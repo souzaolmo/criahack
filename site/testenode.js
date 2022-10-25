5326d90cfdebdb9e84f5a6d9fe72f6af98ac14f1
Júlio Olmo <souzaolmo@gmail.com>
2022-10-25 08:55:21 -0300

    Update testenode.js

--- site/testenode.js
+++ site/testenode.js
@@ -1,19 +1,3 @@
-function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[Erro] Faltam dados')
-    } else {
-       res.innerHTML = 'Contando: <br> '
-       let i = Number(ini.value)
-       let f = Number(fim.value)
-       let p = Number(passo.value)
-
-       for (let c = i; c <= f; c += p) {
-        res.innerHTML += `${c}`
-       }
-    }
+function tabuada() {
+    let num= document.getElementById('txtn')
 }
